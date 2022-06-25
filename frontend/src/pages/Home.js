2fc425e8cbe49fffac0b2b28ae23a9398a558d1d
Ramith <89182652+ramithperera@users.noreply.github.com>
2022-06-25 20:56:51 +0530

    Initial fronted created

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const Home = () => {
+    return(
+        <div>
+            <center>
+                <h1>Welcome to the Home Page</h1>
+                <br/>
+                <br/>
+                <br/>
+                
+            </center>
+        </div>
+    );
+};
+
+export default Home;
